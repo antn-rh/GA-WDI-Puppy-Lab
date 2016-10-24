283e1b36ec18f4951f7f5a2967acbeddea27354d
Antonio Rhee <antoniolrhee@gmail.com>
2016-10-24 12:38:36 -0700

    adds puppies index

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
   root 'puppies#index'
-
-  get 'puppies/' => 'puppies#index'
-  post 'puppies/' => 'puppies#create'
-  get 'puppies/new' => 'puppies#new', as: :new_puppy
-  get 'puppies/:id' => 'puppies#show', as: :puppy
-  get 'puppies/:id/edit' => 'puppies#edit', as: :edit_puppy
-  patch 'puppies/:id' => 'puppies#update'
-  delete 'puppies/:id' => 'puppies#destroy'
+  resources :puppies
+  #
+  # get 'puppies/' => 'puppies#index'
+  # post 'puppies/' => 'puppies#create'
+  # get 'puppies/new' => 'puppies#new', as: :new_puppy
+  # get 'puppies/:id' => 'puppies#show', as: :puppy
+  # get 'puppies/:id/edit' => 'puppies#edit', as: :edit_puppy
+  # patch 'puppies/:id' => 'puppies#update'
+  # delete 'puppies/:id' => 'puppies#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puppies = Puppy.create ([
+  {name: 'Muffin', breed: 'Maltipoo', age: 2, tricks: false},
+  {name: 'Bo', breed: 'Portuguese Water Dog', age: 8, tricks: true},
+  {name: 'Hank', breed: 'Black Lab - Golden Retriever mix', age: 12, tricks: false}
+  ])
